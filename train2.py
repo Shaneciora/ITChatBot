a30b06645223e8e0dc771584f65fd45e75889b0e
Milan Radovanovic <miradova@ucsc.edu>
2020-03-10 18:53:10 -0700

    added 3 questions

--- train2.py
+++ train2.py
@@ -15,3 +15,11 @@ trainer.train([
     "Forgot Password",
     "Please enter your account email and we will send you a verification code to reset your password.",
 ])
+
+trainer.train([
+    "What do you need help with? I can offer help regarding: \n Account Information \n Forgot Password \n System Status",
+    "System Status",
+    "The system is operational, for more information, type 'info'",
+    "info",
+    "<display system information>",
+])
