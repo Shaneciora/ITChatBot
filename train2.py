fb3bc854524870d80f379c0f56bf5228f7a7a4ef
Milan Radovanovic <miradova@ucsc.edu>
2020-03-10 17:56:24 -0700

    added two responses

--- train2.py
+++ train2.py
@@ -5,5 +5,13 @@ trainer = ListTrainer(chatbot)
 
 trainer.train([
     "I need some help.",
-    "What do you need help with? I can offer help regarding:\nAccount Information\nForgot Password\nSystem Status",
+    "What do you need help with? I can offer help regarding: \n Account Information \n Forgot Password \n System Status",
+    "Account Information",
+    "Your account information:",
+])
+
+trainer.train([
+    "What do you need help with? I can offer help regarding: \n Account Information \n Forgot Password \n System Status",
+    "Forgot Password",
+    "Please enter your account email and we will send you a verification code to reset your password.",
 ])
