b424aa52c100a77fdab12657980e64bb7b03b688
Milan Radovanovic <miradova@ucsc.edu>
2020-03-10 18:58:00 -0700

    update

--- train2.py
+++ train2.py
@@ -11,13 +11,11 @@ trainer.train([
 ])
 
 trainer.train([
-    "What do you need help with? I can offer help regarding: \n Account Information \n Forgot Password \n System Status",
     "Forgot Password",
     "Please enter your account email and we will send you a verification code to reset your password.",
 ])
 
 trainer.train([
-    "What do you need help with? I can offer help regarding: \n Account Information \n Forgot Password \n System Status",
     "System Status",
     "The system is operational, for more information, type 'info'",
     "info",
