f09240afadcaa2f83b9c81d9a193090116594558
Milan Radovanovic <miradova@ucsc.edu>
2020-03-11 19:10:22 -0700

    added lowercase options

--- train.py
+++ train.py
@@ -44,3 +44,36 @@ def train_bot():
         "Support Tickets",
         "Here is a list of your support tickets:<br>325678 | <strong>I need to change my email address</strong> | Status: Unresolved <br>329765 | <strong>No Server 2 access</strong> | Status: Resolved",
     ])
+
+#lowercase options
+    trainer2.train([
+        "i need some help",
+        "What do you need help with? I can offer help regarding: <br>| <strong>Account Information</strong> |<br>| <strong>Forgot Password</strong> |<br>| <strong>Server Status</strong> |<br>| <strong>New Support Request</strong> |<br>| <strong>Support Tickets</strong> |",
+        "account information",
+        "Your account information:<br>Username: admin<br>Email: admin@mac.com<br>Account Type: ADMINISTRATOR<br><strong>Manage Information</strong>",
+    ])
+
+    trainer2.train([
+        "forgot password",
+        "Please reply with your account email and we will send you a verification code to reset your password.",
+    ])
+
+    trainer2.train([
+        "server status",
+        "Your server (Server 1) is online, for more information, type 'List Servers'",
+    ])
+
+    trainer2.train([
+        "list servers",
+        "User: admin <br>Pass: ***** <br><strong>Server 1</strong> | Status: Online | Ping: 86 |<br><strong>Server 2</strong> | Status: Online | Ping: 130 |<br><strong>TEST Server</strong> | Status: Offline | Ping: N/A |",
+    ])
+
+    trainer2.train([
+        "new support request",
+        "Follow this link to create a new support request: www.company.com/newsupportticket",
+    ])
+
+    trainer2.train([
+        "support tickets",
+        "Here is a list of your support tickets:<br>325678 | <strong>I need to change my email address</strong> | Status: Unresolved <br>329765 | <strong>No Server 2 access</strong> | Status: Resolved",
+    ])
